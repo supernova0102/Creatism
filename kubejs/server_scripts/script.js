ad5df2503bd6b1521305f4841e9a1d245d3e981b
supernova0102 <lskojy@gmail.com>
2023-03-30 00:52:43 +0900

    build 3/30
    이제 kubeJS로 옮겨갈 예정
    코블젠 커스텀 추가
    
    이그니우스익스트루더에 버그있음

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -2,6 +2,19 @@
 
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
+
+//전역변수 선언
+var custom_gen_stones = 
+		['minecraft:granite','minecraft:diorite','minecraft:andesite','minecraft:andesite','create:asurine','create:crimsite','create:ochrum','create:veridium','minecraft:calcite','minecraft:tuff'];
+var c1core = "immersiveengineering:light_engineering";
+var baseMachineCore = "create:andesite_casing";
+var c2core = "immersiveengineering:heavy_engineering";
+var c3core = "thermal:machine_frame";
+var c4Core = "compressedcreativity:compressed_iron_casing";
+var transCore = "create:copper_casing";
+
+
+
 ServerEvents.entityLootTables(event => {
 	event.modifyEntity("minecraft:cow", table => {
 		table.addPool(pool => {
@@ -21,14 +34,9 @@ ServerEvents.entityLootTables(event => {
 })
 
 ServerEvents.recipes(event => {
-	event.recipes.createMechanicalExtruderExtruding(Item.of('minecraft:andesite'),[Fluid.of('minecraft:water'),Fluid.of('minecraft:lava')]).withCatalyst('minecraft:sculk_catalyst')
-	event.recipes.createMechanicalExtruderExtruding(Item.of('minecraft:andesite'),[Fluid.of('minecraft:water'),Fluid.of('minecraft:lava')]).withCatalyst('minecraft:sculk')
-	event.recipes.createMechanicalExtruderExtruding(Item.of('minecraft:andesite'),[Fluid.of('minecraft:water'),Fluid.of('minecraft:lava')]).withCatalyst('minecraft:bedrock')
+	//레시피변경
+	c1Recipes();
 
-	event.recipes.createMechanicalExtruderExtruding(Item.of('minecraft:dripstone_block'),[Fluid.of('minecraft:water'),Fluid.of('minecraft:lava')]).withCatalyst('minecraft:sculk_catalyst')
-	event.recipes.createMechanicalExtruderExtruding(Item.of('minecraft:dripstone_block'),[Fluid.of('minecraft:water'),Fluid.of('minecraft:lava')]).withCatalyst('minecraft:sculk')
-	event.recipes.createMechanicalExtruderExtruding(Item.of('minecraft:dripstone_block'),[Fluid.of('minecraft:water'),Fluid.of('minecraft:lava')]).withCatalyst('minecraft:bedrock')
-	
 	event.recipes.createoreexcavation.drilling('immersiveengineering:raw_aluminum', '{"text": "Bauxite vein"}', 50, 400).id("my_vein1");
 	event.recipes.createoreexcavation.drilling('thermal:raw_silver', '{"text": "Raw Silver vein"}', 20, 400).id("my_vein2");
 	event.recipes.createoreexcavation.drilling('thermal:raw_nickel', '{"text": "Raw Nickel vein"}', 30, 400).id("my_vein3");
@@ -39,13 +47,41 @@ ServerEvents.recipes(event => {
 	event.recipes.createoreexcavation.drilling('thermal:cinnabar', '{"text": "Cinnabar vein"}', 10, 100).id("my_vein7");
 	event.recipes.createoreexcavation.drilling('thermal:sulfur', '{"text": "Sulfur vein"}', 40, 100).id("my_vein8");
 	event.recipes.createoreexcavation.drilling('thermal:niter', '{"text": "Niter vein"}', 50, 100).id("my_vein9");
-	// Change recipes here
+	
+
+	//커스텀 레시피함수
+	function custom_rockgen(output, catalyst){
+		event.custom({
+			"type" : "thermal:rock_gen",
+			"abjacent" : 'minecraft:ice',
+			"below" : catalyst,
+			"result" : { "item" : output}
+		});
+		event.recipes.createMechanicalExtruderExtruding(Item.of(output),[Fluid.of('minecraft:water'),Fluid.of('minecraft:lava')])
+		.withCatalyst(catalyst);
+	}
+
+	//레시피 등록 파편화
+	function c1Recipes(){
+		for(let item of custom_gen_stones){
+			custom_rockgen(item, 'minecraft:sculk');
+			custom_rockgen(item, 'minecraft:sculk_catalyst');
+			custom_rockgen(item, 'minecraft:bedrock');
+		}
+
+		
+	}
+
 })
 
 ServerEvents.tags('item', event => {
+	
+
 	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
 	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
 
 	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
 	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 })
+
+//레시피 변경 파편화
